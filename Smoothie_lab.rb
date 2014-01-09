e6cf0021c54b51cc7475d9cafa4e71d223f7374d
mbetts7 <mbetts7@gmail.com>
2014-01-08 16:52:40 -0800

    initial setup

---
+++ Smoothie_lab.rb
@@ -0,0 +1,44 @@
+# mix up all the ingredient characters in the keys and remove the spaces
+# method for getting keys out of a hash
+# randomly chose 3 ingredients and blended it
+
+
+
+
+
+
+
+
+# Every Morning I make a smoothie with the follow ingredients:
+smoothie_ingredients = {
+  'flax seeds' => '1 tbsp',
+  'chia seeds' => '1 tbsp',
+  'coconut flakes' => '1 tbsp',
+  'spirulina' => '1 tsp',
+  'pumpkin seeds' => '1 tbsp',
+  'oatmeal' => '1/4 cup',
+  'coco powder' => '1 tbsp',
+  'peanut butter' => '1 tbsp',
+  'almonds' => '1/4 cup',
+  'walnuts' => '1/4 cup',
+  'spinach' => '1/4 cup',
+  'greek yogurt' => '1/4 cup',
+  'nutrional yeast' => '1 tbsp',
+  'brussel sprouts' => '1/4 cup',
+  'asparagus' => '1/4 cup',
+  'kale' => '1/4 cup',
+  'brocoli rabe' => '1/4 cup',
+  'blue berries' => '1/4 cup',
+  'chopped banana' => '1/2 cup',
+  'straw berries' => '1/4 cup',
+  'mango' => '1/4 cup',
+  'hemp milk' => '1 cup'
+}
+
+def blend(smoothie_ingredients)
+
+end
+
+
+class Blender
+
