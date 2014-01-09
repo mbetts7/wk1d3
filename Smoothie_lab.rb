2b83c6434ba62b7094197116de5b5ac82594030e
mbetts7 <mbetts7@gmail.com>
2014-01-08 19:55:01 -0800

    finally there, phew

--- Smoothie_lab.rb
+++ Smoothie_lab.rb
@@ -2,13 +2,6 @@
 # method for getting keys out of a hash
 # randomly chose 3 ingredients and blended it
 
-
-
-
-
-
-
-
 # Every Morning I make a smoothie with the follow ingredients:
 smoothie_ingredients = {
   'flax seeds' => '1 tbsp',
@@ -35,10 +28,58 @@ smoothie_ingredients = {
   'hemp milk' => '1 cup'
 }
 
-def blend(smoothie_ingredients)
+# take all ingredients
+# Save those ingredient keys as ones string
+# delete spaces -- use .delete(" ")
+# jumble string
+# returns output of mixed up characters
 
+
+# create a class called Blender
+# It should have a method that takes an array of ingredients and returns a mixed string of characters.
+# Give the blender an on and off switch and only allow the blender to function when it's on.
+# FOR SAFETY'S SAKE When you create a new blender by default it should be off.
+# Blend the the smoothie array
+
+class Blender  # define a class recipe for object Blender
+
+  attr_accessor :status  # enable read/write access
+  def initialize(status="off")  # initialize method with variable status by default set to off
+    @status = status # instantiate the variable status
+  end
+
+  def switch  
+    if @status == "off"
+      @status = "on"
+    else
+      @status = "off"
+    end
+  end  
+
+  def blend(smoothie_ingredients)
+      ingredients_blend = []
+      ingredients_blend = smoothie_ingredients.keys.shuffle.join.delete(" ")
+      smoothie_ingredients = ingredients_blend
+  end
+end
+
+blender1 = Blender.new
+puts "Your blender is #{blender1.status}. Enter 'on' to turn it on."
+res = gets.chomp
+until res == 'off'
+  if res == 'on'
+      puts blender1.blend(smoothie_ingredients)
+      puts "To blend again, enter 'b'.  To turn off, enter 'off'."
+      res = gets.chomp
+    elsif res == 'b'
+      puts blender1.blend(smoothie_ingredients)
+      puts "To blend again, enter 'b'.  To turn off, enter 'off'."
+      res = gets.chomp
+    else
+  end
+  puts "Your blender is #{blender1.status}."
 end
 
 
-class Blender
+
 
